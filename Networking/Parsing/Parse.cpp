28fd0cdb503dbc164dcc5b07edfd8cc55e5479af
Mikko Numminen <minummin@shi-r3-p6.s19.be>
2021-12-30 20:28:11 +0100

    kk

--- Networking/Parsing/Parse.cpp
+++ Networking/Parsing/Parse.cpp
@@ -123,17 +123,17 @@ void    Parse::setConfs()
         n--;
     }
 
-    for (std::vector<std::map<std::string, std::vector<std::string> > >::iterator it = _conf_vect.begin(); it != _conf_vect.end(); it++)
-    {
-        for (std::map<std::string, std::vector<std::string> >::iterator it2 = it->begin(); it2 != it->end(); it2++)
-        {
-            std::cout << it2->first << " ";
-            for (std::vector<std::string>::iterator it3 = it2->second.begin(); it3 != it2->second.end(); it3++)
-                std::cout << *it3 << " ";
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-    }
+    // for (std::vector<std::map<std::string, std::vector<std::string> > >::iterator it = _conf_vect.begin(); it != _conf_vect.end(); it++)
+    // {
+    //     for (std::map<std::string, std::vector<std::string> >::iterator it2 = it->begin(); it2 != it->end(); it2++)
+    //     {
+    //         std::cout << it2->first << " ";
+    //         for (std::vector<std::string>::iterator it3 = it2->second.begin(); it3 != it2->second.end(); it3++)
+    //             std::cout << *it3 << " ";
+    //         std::cout << std::endl;
+    //     }
+    //     std::cout << std::endl;
+    // }
 }
 
 void    Parse::_error_manage(std::string str)
