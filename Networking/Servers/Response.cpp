c8e74a37e6e59d5d0519b20a894fa55d71616e6c
Mikko Numminen <minummin@fu-r2-p5.s19.be>
2022-01-11 17:43:37 +0100

    kk

--- Networking/Servers/Response.cpp
+++ Networking/Servers/Response.cpp
@@ -8,12 +8,12 @@ void    Response::_handler(int clientSocket, struct Parse::serverBlock server)
     int size;
     int bytes_sending;
     std::string type;
+    char buffer[BUFF_SIZE] = {0};
 
     _errorCode = 200;
-    memset(_buffer, 0, sizeof(_buffer));
-    recv(clientSocket, _buffer, sizeof(_buffer), 0);
-    std::cout << _buffer << std::endl;
-    std::istringstream iss(_buffer);
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << buffer << std::endl;
+    std::istringstream iss(buffer);
     std::vector<std::string> parsed((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
     type = parsed[1].substr(parsed[1].rfind(".") + 1, parsed[1].size() - parsed[1].rfind("."));
 
@@ -93,103 +93,4 @@ std::string     Response::_getContent(std::vector<std::string> parsed, std::stri
             content = _getDefaultFile(type);
     }
     return content;
-}
-std::string Response::_get413(std::string *type)
-{
-    std::string content;
-    std::ifstream f("./www/413.html");
-    if (f.good())
-    {
-        std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
-        content = str;
-        _errorCode = 413;
-        *type = "html";
-    }
-    f.close();
-    return content;
-}
-
-std::string Response::_getFile(std::ifstream *f)
-{
-    std::string str((std::istreambuf_iterator<char>(*f)), std::istreambuf_iterator<char>());
-    std::string content = str;
-    _errorCode = 200;
-    return content;
-}
-
-std::string Response::_get404(std::string *type)
-{
-    std::string content;
-    std::ifstream f2("./www/404.html");
-    if (f2.good())
-    {
-        std::string str((std::istreambuf_iterator<char>(f2)), std::istreambuf_iterator<char>());
-        content = str;
-        _errorCode = 404;
-        *type = "html";
-    }
-    f2.close();
-    return content;
-}
-
-std::string Response::_getDefaultFile(std::string *type)
-{
-    std::string content;
-    std::ifstream f("./www/default.html");
-    if (f.good())
-    {
-        std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
-        content = str;
-        _errorCode = 200;
-        *type = "html";
-    }
-    f.close();
-    return content;
-}
-
-
-std::string     Response::_getContentLength(int size)
-{
-    std::ostringstream oss;
-
-    oss << "Content-Length: ";
-    oss << size << "\r\n";
-    return oss.str();
-}
-
-std::string     Response::_getCacheControl()
-{
-    std::string str;
-
-    str = "Cache-Control: no-cache, private\r\n";
-    return str;
-}
-
-std::string     Response::_getStatus(int err_code)
-{
-    if (err_code == 200)
-        return " OK\r\n";
-    else if (err_code == 413)
-        return "Payload Too Large\r\n";
-    else
-        return " Not Found\r\n";
-}
-
-std::string     Response::_getContentType(std::string type)
-{
-    std::string str;
-
-    str = "Content-Type: ";
-    if (type == "jpeg" || type == "jpg")
-        str += "image/jpeg";
-    else if (type == "html" || type == "/")
-        str += "text/html";
-    else if (type == "png")
-        str += "image/png";
-    else if (type == "bmp")
-        str += "image/bmp";
-    else
-        str += "text/plain";
-    str += "\r\n";
-    return str;
 }
--- Networking/Utils/Utils.hpp
+++ Networking/Utils/Utils.hpp
@@ -11,6 +11,11 @@ class Utils
         std::vector<unsigned char>  readBinaryFile(char *conf, std::string path);
 
     protected:
+        int             _errorCode;
+        std::string     _root;
+        std::string     _index;
+        int             _max_size;
+
         void    _check_body_size(std::string *x);
         void    _check_listen(std::vector<std::string> *v);   
         int     _ft_isprint(int c);
@@ -22,6 +27,15 @@ class Utils
         void    _check_method(std::vector<std::string> *v);
         void    _check_autoindex(std::string *x);
         void    _check_error_page(std::map<int, std::string> *m);
+
+        std::string     _get404(std::string *type);
+        std::string     _get413(std::string *type);
+        std::string     _getFile(std::ifstream *f);
+        std::string     _getDefaultFile(std::string *type);
+        std::string     _getCacheControl();
+        std::string     _getContentType(std::string type);
+        std::string     _getContentLength(int size);
+        std::string     _getStatus(int err_code);
 };
 
 #endif
